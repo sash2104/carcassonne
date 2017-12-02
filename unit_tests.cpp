690704d374f236b75484c962e8dc4ee6294f1be4
Yuki Tawara <yktawara@gmail.com>
2017-12-02 19:28:51 +0900

    Write test for CityRegion

--- unit_tests.cpp
+++ unit_tests.cpp
@@ -5,6 +5,7 @@
 #include "segment.hpp"
 #include "test_util.hpp"
 #include "tile.hpp"
+#include "tile_factory.hpp"
 #include "tile_position_map.hpp"
 
 void tile_position_map_tests() {
@@ -28,6 +29,29 @@ void tile_position_map_tests() {
   s = m.getPlacablePositions();
   test_assert("tile_position_map_tests#8", s->size() == 6);
 }
+
+void cup_city_tests(TileFactory* tile_f) {
+  Tile* tile01 = tile_f->newFromName("E", 0);
+  Tile* tile02 = tile_f->newFromName("E", 1);
+  tile01->setX(0).setY(0).setRotation(2);
+  tile02->setX(0).setY(-1).setRotation(0);
+  CityRegion region(0, nullptr);
+  region.addSegment(tile01->getCitySegments()->at(0));
+  test_assert("cup_city_tests#0", !region.isCompleted());
+  test_assert("cup_city_tests#1", region.calculatePoint() == 1);
+  region.addSegment(tile02->getCitySegments()->at(0));
+  test_assert("cup_city_tests#2", region.isCompleted());
+  test_assert("cup_city_tests#3", region.calculatePoint() == 4);
+  delete tile01;
+  delete tile02;
+}
+
+void city_region_tests() {
+  TileFactory tile_f;
+  tile_f.loadResource("tiles.json");
+  cup_city_tests(&tile_f);
+}
+
 
 void cloister_region_tests() {
   Board board(10);
@@ -60,6 +84,7 @@ void cloister_region_tests() {
 void tests() {
   tile_position_map_tests();
   cloister_region_tests();
+  city_region_tests();
 }
 
 int main(int argc, char* argv[]) {
