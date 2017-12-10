0583f1221780a91cab14a9f8517b3a60350cb40c
Yuki Tawara <yktawara@gmail.com>
2017-12-10 14:06:13 +0900

    Use static array to calculate adjacent coordinate

--- utils.hpp
+++ utils.hpp
@@ -14,28 +14,12 @@ inline int modBy8(int n) {
 
 inline std::pair<int, int> adjacentCoordinate(int x, int y, int direction) {
   assert(direction >= 0 && direction < 4);
+  const static int dx[4] = {0, 1, 0, -1};
+  const static int dy[4] = {1, 0, -1, 0};
   std::pair<int, int> p;
-  switch (direction) {
-  case 0:
-    p.first = x;
-    p.second = y + 1;
-    return p;
-  case 1:
-    p.first = x + 1;
-    p.second = y;
-    return p;
-  case 2:
-    p.first = x;
-    p.second = y - 1;
-    return p;
-  case 3:
-    p.first = x - 1;
-    p.second = y;
-    return p;
-  default:
-    // ここには到達しない
-    assert(0);
-  }
+  p.first = x + dx[direction];
+  p.second = y + dy[direction];
+  return p;
 }
 
 #endif
